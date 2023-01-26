93e49fd87ef612964ace5dfd487146f73235530e
Park-gyunam <119722854+Park-gyunam@users.noreply.github.com>
2023-01-26 11:10:44 +0900

    [Silver IV] Title: 제로, Time: 4848 ms, Memory: 31248 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,10 @@
+n = int(input())
+num_list = []
+
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        num_list.pop()
+    else:
+        num_list.append(num)
+print(sum(num_list))
