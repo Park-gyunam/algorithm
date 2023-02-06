6a56032427e93d46aab9cd92ad3dade43b31161a
Park-gyunam <119722854+Park-gyunam@users.noreply.github.com>
2023-02-06 17:23:39 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,23 @@
+n = int(input()) # 컴퓨터 수
+m = int(input()) # 간선 수
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for _ in range(m):
+    v1, v2 = map(int, input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+def dfs(start):
+    stack = [start]
+    visited[start] = True
+
+    while stack:
+        cur = stack.pop()
+
+        for adj in graph[cur]:
+            if not visited[adj]:
+                visited[adj] = True
+                stack.append(adj)
+dfs(1)
+print(sum(visited) - 1)
